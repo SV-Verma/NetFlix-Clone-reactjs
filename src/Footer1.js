df707ac73c0bab97c3822937eb0c11ab06995fe0
shivangi <shivangivv07@gmail.com>
2023-09-01 23:44:02 +0530

    changing

--- src/Footer1.js
+++ src/Footer1.js
@@ -8,7 +8,7 @@ function Footer1() {
         <div className="footer1-container">
           <div className="footer1-heading">
            "Question? Call "
-           <a href="">
+           <a href="/">
            000-800-919-1694
            </a>
           </div>
@@ -17,78 +17,78 @@ function Footer1() {
             <div className="footer1-list-name">
               <ul>
                 <li className="footer1-listed-name">
-                  <a href=""
+                  <a href="{null}"
                   className='footer1-a'
                   >
                     FAQ</a>
                 </li>
                 <li className="footer1-listed-name">
-                  <a href="/"
+                  <a href="{null}"
                   className='footer1-a'
                   >Help Centre</a>
                 </li>
                 <li className="footer1-listed-name">
-                  <a href=""
+                  <a href="{null}"
                   className='footer1-a'>
                     Account</a>
                 </li>
                 <li className="footer1-listed-name">
-                  <a href=""
+                  <a href="{null}"
                   className='footer1-a'>
                     Media Centre</a>
                 </li>
                 <li className="footer1-listed-name">
-                  <a href=""
+                  <a href="{null}"
                   className='footer1-a'>
                     Investor Relations</a>
                 </li>
                 <li className="footer1-listed-name">
-                  <a href=""
+                  <a href="{null}"
                   className='footer1-a'>
                     Jobs</a>
                 </li>
                 <li className="footer1-listed-name">
-                  <a href=""
+                  <a href="{null}"
                   className='footer1-a'>
                     Ways to Watch</a>
                 </li>
                 <li className="footer1-listed-name">
-                  <a href=""
+                  <a href="{null}"
                   className='footer1-a'>
                     Terms of Use</a>
                 </li>
                 <li className="footer1-listed-name">
-                  <a href=""
+                  <a href="{null}"
                   className='footer1-a'>
                     Privacy</a>
                 </li>
                 <li className="footer1-listed-name">
-                  <a href=""
+                  <a href="{null}"
                   className='footer1-a'>
                     Cookie Preferences</a>
                 </li>
                 <li className="footer1-listed-name">
-                  <a href=""
+                  <a href="{null}"
                   className='footer1-a'>
                     Corporate Information</a>
                 </li>
                 <li className="footer1-listed-name">
-                  <a href=""
+                  <a href="{null}"
                   className='footer1-a'>
                     Contact Us</a>
                 </li>
                 <li className="footer1-listed-name">
-                  <a href=""
+                  <a href="{null}"
                   className='footer1-a'>
                     Speed Test</a>
                 </li>
                 <li className="footer1-listed-name">
-                  <a href="" 
+                  <a href="{null}" 
                   className='footer1-a'>
                     Legal Notices</a>
                 </li>
                 <li className="footer1-listed-name">
-                  <a href="" 
+                  <a href="{null}" 
                   className='footer1-a'>
                     Only on Netflix</a>
                 </li>
